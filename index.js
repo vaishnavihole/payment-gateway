b592ee7b1b4545ff0b471dd545ecef135f5ab1ef
vaishnavihole <vaishnavihole1@gmail.com>
2023-03-24 11:22:52 +0530

    import files

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from "express";
+var bodyParser = require("body-parser");
+const ejs = require('ejs');
 
 const app = express();
 app.use(express.json())
 
 const PORT = 5000;
 
+app.use(express_static(__dirname + '/views'))
+app.engine('html', require('ejs').renderfile);
+app.set('view engine',"html");
+app.set("views", _dirname + '/views');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 app.listen(PORT, ()=>{
 console.log(`Server started on Port ${PORT}`)
 })
+
+// ejs help us to build html templatesin node
+// embeded javascript templates
