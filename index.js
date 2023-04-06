4f4fa5bbc707bdacc4fad283aa20619490605b34
vaishnavihole <vaishnavihole1@gmail.com>
2023-04-06 11:54:14 +0530

    basic setup added

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 import express from "express";
-var bodyParser = require("body-parser");
-const ejs = require('ejs');
+
 
 const app = express();
 app.use(express.json())
 
 const PORT = 5000;
 
-app.use(express_static(__dirname + '/views'))
-app.engine('html', require('ejs').renderfile);
-app.set('view engine',"html");
-app.set("views", _dirname + '/views');
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
 
-app.use('/', require('./routes/server'))
-
-app.listen(PORT, ()=>{
-console.log(`Server started on Port ${PORT}`)
+app.get("/", (req, res) => {
+    res.sendFile("Hello World!");
 })
-
-// ejs help us to build html templatesin node
-// embeded javascript templates
+app.listen(PORT, () => {
+    console.log(`Server started on Port ${PORT}`)
+})
