df59e06eb79434179a2729fa64836b640231bdb5
vaishnavihole <vaishnavihole1@gmail.com>
2023-04-06 17:29:56 +0530

    post request added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import Razorpay from "razorpay";
 
 
 const app = express();
@@ -9,6 +10,18 @@ const PORT = 5000;
 
 app.get("/", (req, res) => {
     res.sendFile("Hello World!");
+});
+
+app.post("/payment", async(req, res) => {
+      var instance = new Razorpay({key_id: '',key_secret: '',})
+      instance.orders.create[{
+        amount: 50000,
+        currency: "receipt#1",
+        notes: {
+            key1: "value3",
+            key2: "value2"
+        }
+      }]
 })
 app.listen(PORT, () => {
     console.log(`Server started on Port ${PORT}`)
